5c18b53169b661aabe5188f71a330d760283e269
buneuk <144821528+buneuk@users.noreply.github.com>
2024-05-05 14:32:24 +0100

    Update config.lua

--- [BW-deathdrop]/config.lua
+++ [BW-deathdrop]/config.lua
@@ -1,7 +1,5 @@
 Config = {}
 
-Config.Framework = 'ESX'  -- Only Works For ESX Right Now! 
-
 Config.includeWeaponsInDrop = true -- Include weapons in drop?
 Config.includeItemsToDrop = true -- Include additional items in drop?
 
